1faf378fe582ac8cfe40f9a30702938b794113b4
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2021-12-28 14:17:30 +0100

    Create task0430.java

---
+++ task0430.java
@@ -0,0 +1,16 @@
+package en.codegym.task.jdk13.task04.task0430;
+
+/* 
+10 numbers
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        int x = 1;
+        while (x <= 10){
+            System.out.println(x);
+            x++;
+        }
+    }
+}
