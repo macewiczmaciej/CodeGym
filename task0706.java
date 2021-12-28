1b43d963e058e17d2e96c10dfc878e0c6a836f62
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2021-12-28 14:26:33 +0100

    Update task0706.java

--- task0706.java
+++ task0706.java
@@ -1,33 +1,35 @@
-package en.codegym.task.jdk13.task07.task0705;
+package en.codegym.task.jdk13.task07.task0706;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /* 
-One large array and two small ones
+Street and houses
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         //write your code here
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] list = new int[20];
-        for (int i = 0; i < 20; i++) {
-            list[i] = Integer.parseInt(reader.readLine());
-        }
-        int[] list1 = new int[10];
-        int[] list2 = new int[10];
+        int[] array = new int[15];
 
-        for (int i = 0; i < list.length; i++) {
-            if (i < list.length / 2) {
-                list1[i] = list[i];
+        int sumOdd = 0;
+        int sumEven = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+            if (i % 2 == 1) {
+                sumOdd += array[i];
             } else {
-                list2[i - list.length / 2] = list[i];
+                sumEven += array[i];
             }
         }
-        for (int i = 0; i < list2.length; i++) {
-            System.out.println(list2[i]);
+        if (sumEven > sumOdd) {
+            System.out.println("Even-numbered houses have more residents.");
+        } else if (sumEven < sumOdd) {
+            System.out.println("Odd-numbered houses have more residents.");
         }
     }
 }
