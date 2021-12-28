1d7a926a4fddbe7f8e9921c8c2f8bdb96f758262
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2021-12-28 14:21:01 +0100

    Create task0438.java

---
+++ task0438.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task04.task0438;
+
+/* 
+Drawing lines
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        for (int i = 0; i < 10; i++) {
+            System.out.print(8);
+        }
+        System.out.println();
+        for (int j = 0; j < 10; j++) {
+            System.out.println(8);
+        }
+    }
+}
