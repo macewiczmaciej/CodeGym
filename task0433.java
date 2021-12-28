6840d8ee397f0fb80bcf2673f1c1b6e1e6e1ce01
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2021-12-28 14:18:56 +0100

    Create task0433.java

---
+++ task0433.java
@@ -0,0 +1,19 @@
+package en.codegym.task.jdk13.task04.task0433;
+
+/* 
+Seeing dollars in your future
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        String signs = "$$$$$$$$$$";
+        int n = 10;
+
+        while(n>0){
+            System.out.println(signs);
+            n--;
+        }
+
+    }
+}
