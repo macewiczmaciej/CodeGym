ff46d4f37fee7bd354636fd4a1c0e332396fab39
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2021-12-28 14:20:50 +0100

    Create task0437.java

---
+++ task0437.java
@@ -0,0 +1,17 @@
+package en.codegym.task.jdk13.task04.task0437;
+
+/* 
+Triangle of eights
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
+    }
+}
