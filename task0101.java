5b3163b548b297aaf05db451f81ea410bfe82a74
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2021-12-28 14:17:01 +0100

    Create task0101.java

---
+++ task0101.java
@@ -0,0 +1,14 @@
+package en.codegym.task.jdk13.task01.task0101;
+  
+/* 
+It's cool to be a programmer!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        System.out.println("I think being a programmer is cool");
+        
+        
+    }
+} 
