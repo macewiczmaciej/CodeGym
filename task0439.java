dfea37daf1236badcb0c7af41d2651ba2820a765
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2021-12-28 14:22:31 +0100

    Create task0439.java

---
+++ task0439.java
@@ -0,0 +1,21 @@
+package en.codegym.task.jdk13.task04.task0439;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Chain letter
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = reader.readLine();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name+" loves me.");
+        }
+    }
+}
