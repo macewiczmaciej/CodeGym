391d763217d457f858931dbf18474a65af142e57
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2021-12-28 14:19:35 +0100

    Create task0435.java

---
+++ task0435.java
@@ -0,0 +1,17 @@
+package en.codegym.task.jdk13.task04.task0435;
+
+/* 
+Even numbers
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        for(int i = 1;i<=100;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
