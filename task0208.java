94c9a8c438441f756d6dbd822c82668517fdc311
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-02 21:09:01 +0100

    Create task0208.java

---
+++ task0208.java
@@ -0,0 +1,17 @@
+package en.codegym.task.jdk13.task02.task0208;
+
+/* 
+Cats are good
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
