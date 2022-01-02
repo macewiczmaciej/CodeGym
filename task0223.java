d9f18bc73a87b6a46084ccb45e6512e142599797
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-02 21:24:31 +0100

    Create task0223.java

---
+++ task0223.java
@@ -0,0 +1,13 @@
+package en.codegym.task.jdk13.task02.task0223;
+
+/* 
+Variety
+*/
+
+public class Solution {
+    //write your code here
+    int x = 1;
+    int y = 2;
+    int a, b;
+    int e = 3, r = 4;
+}
