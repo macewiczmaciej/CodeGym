73dd80563f756bd53a2b76916af5dc654449d08e
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-02 18:04:18 +0100

    Create task0606.java

---
+++ task0606.java
@@ -0,0 +1,46 @@
+package en.codegym.task.pro.task06.task0606;
+
+/* 
+A method for all seasons
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int x) {
+
+    }
+    public static void universalMethod(int x, String y) {
+
+    }
+    public static void universalMethod(String x) {
+
+    }
+    public static void universalMethod(double x) {
+
+    }
+    public static void universalMethod(short x) {
+
+    }
+    public static void universalMethod(String z, String x) {
+
+    }
+    public static void universalMethod(double p, String z) {
+
+    }
+    public static void universalMethod(float s, String a) {
+
+    }
+    public static void universalMethod(float b) {
+
+    }
+    public static void universalMethod(int x, float y, double z) {
+
+    }
+
+
+    //write your code here
+}
