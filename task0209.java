047ff1773b60bfad902d25e71ab6fbdabefcc4ef
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-02 21:21:37 +0100

    Create task0209.java

---
+++ task0209.java
@@ -0,0 +1,21 @@
+package en.codegym.task.jdk13.task02.task0209;
+
+/* 
+Three dogs are a force
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.name = "Max";
+        Dog dog2 = new Dog();
+        dog2.name = "Bella";
+        Dog dog3 = new Dog();
+        dog3.name = "Jack";
+        //write your code here
+    }
+
+    public static class Dog {
+        public String name;
+    }
+}
