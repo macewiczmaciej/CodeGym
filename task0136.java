fc4e3b4656cd2f157eaadeadc3457c6df3d1f018
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-02 18:39:36 +0100

    Create task0136.java

---
+++ task0136.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task01.task0136;
+
+/* 
+Even to the moon!
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getWeight(888));
+    }
+
+    public static double getWeight(int earthWeight) {
+        // write your code here
+        double result = earthWeight * 0.17;
+        return result;
+    }
+}
