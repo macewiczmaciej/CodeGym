69bb1791d67a23ea2b3dbc1b34aefdd2b3438a48
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-04 18:55:05 +0100

    Create task0914.java

---
+++ task0914.java
@@ -0,0 +1,24 @@
+package en.codegym.task.pro.task09.task0914;
+
+/* 
+Path update
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin";
+
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
+    }
+
+    public static String changePath(String path, String jdk) {
+        //write your code here
+        int index1 = path.indexOf("jdk");
+        int index2 = path.indexOf("/",index1);
+        String oldJdk = path.substring(index1,index2);
+        String result = path.replaceFirst(oldJdk,jdk);
+
+        return result;
+    }
+}
