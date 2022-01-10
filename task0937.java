847129fe8974f84b5125c19372872b63ffefb1bf
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-10 22:37:41 +0100

    Create task0937.java

---
+++ task0937.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task09.task0937;
+
+/* 
+Literals
+*/
+
+public class Solution {
+    public byte b = 0;
+    public short s = 123;
+    public int i = 1111;
+    public long l = 2222L;
+    public float f = 34.2f;
+    public double d = 653.89d;
+    public char c = 'J';
+    public boolean boo = true;
+
+    public String string = "Hello";
+}
