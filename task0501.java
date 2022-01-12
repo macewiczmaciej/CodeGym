264ed3293bd2b86f852b9399f9f8245c6fec265d
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-12 22:57:52 +0100

    Create task0501.java

---
+++ task0501.java
@@ -0,0 +1,19 @@
+package en.codegym.task.jdk13.task05.task0501;
+
+/* 
+Cat carnage (1)
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Cat {
+        //write your code here
+        String name;
+        int age;
+        int weight;
+        int strength;
+    }
+}
