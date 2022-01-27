1ec48b90967e79136c14ffc3cbf7c57f94c52005
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-27 17:22:02 +0100

    Create Populate_the_Dog_class.java

---
+++ Populate_the_Dog_class.java
@@ -0,0 +1,31 @@
+package en.codegym.task.jdk13.task05.task0511;
+
+/* 
+Populate the Dog class
+*/
+
+public class Dog {
+    //write your code here
+    private String name;
+    private int height;
+    private String color;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public void initialize(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
