8aa8be5919db32a5076046a7af329b68d8aea7db
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-27 17:45:51 +0100

    Create Initializing_objects_2.java

---
+++ Initializing_objects_2.java
@@ -0,0 +1,25 @@
+package en.codegym.task.jdk13.task05.task0515;
+
+/* 
+Initializing objects
+*/
+
+public class Person {
+    String name;
+    char sex;
+    int money;
+    int weight;
+    double size;
+
+    public void initialize(String name, int money, char sex, int weight, double size) {
+        this.name = name;
+        this.money = money;
+        this.sex = sex;
+        this.weight = weight;
+        this.size = size;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
