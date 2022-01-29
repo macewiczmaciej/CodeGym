1ef7c79fa6765d5a6df444f0d4352308a647ea3b
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-29 19:49:16 +0100

    Create You_cant_buy_friends.java

---
+++ You_cant_buy_friends.java
@@ -0,0 +1,31 @@
+package en.codegym.task.jdk13.task05.task0516;
+
+/* 
+You can't buy friends
+*/
+
+public class Friend {
+    String name;
+    int age;
+    char sex;
+
+    public Friend(String name) {
+        this.name = name;
+    }
+
+    public Friend(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Friend(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+    //write your code here
+
+    public static void main(String[] args) {
+
+    }
+}
