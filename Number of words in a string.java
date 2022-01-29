72ca93a497c8750584c33af1b38f380f24c9e264
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-29 22:27:22 +0100

    Create Number of words in a string.java

---
+++ Number of words in a string.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task09.task0932;
+
+/* 
+Number of words in a string
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Who wants a job?";
+        int getWordCount = getWordCount(string);
+        System.out.println("The number of words in the string \"" + string + "\" is " + getWordCount);
+    }
+
+    public static int getWordCount(String string) {
+        //write your code here
+       return string.split(" ").length;
+    }
+}
