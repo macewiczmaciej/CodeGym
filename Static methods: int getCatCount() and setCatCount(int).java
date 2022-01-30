552e5d84419857344d56f40d6de413eddd856751
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-30 21:43:03 +0100

    Create Static methods: int getCatCount() and setCatCount(int).java

---
+++ Static methods: int getCatCount() and setCatCount(int).java
@@ -0,0 +1,28 @@
+package en.codegym.task.jdk13.task06.task0608;
+
+/* 
+Static methods: int getCatCount() and setCatCount(int)
+*/
+
+public class Cat {
+    private static int catCount = 0;
+
+    public Cat() {
+        catCount++;
+    }
+
+    public static int getCatCount() {
+        //write your code here
+        return catCount;
+
+    }
+
+    public static void setCatCount(int catCount) {
+        //write your code here
+        Cat.catCount = catCount;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
