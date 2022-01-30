d0199a76653c424bf7efb96e95b92f21511d289f
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-30 21:40:11 +0100

    Create Class counter.java

---
+++ Class counter.java
@@ -0,0 +1,18 @@
+package en.codegym.task.jdk13.task06.task0607;
+
+/* 
+Class counter
+*/
+
+public class Cat {
+    //write your code here
+    public static int catCount;
+
+    public Cat() {
+        catCount += 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
