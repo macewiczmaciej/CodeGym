41b8281f70e30e1dedad70ad963e7339323a9fc0
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-01-30 21:59:46 +0100

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+package en.codegym.task.jdk13.task06.task0612;
+
+/* 
+Calculator
+*/
+
+public class Calculator {
+    public static int plus(int a, int b) {
+        //write your code here
+        return a+b;
+    }
+
+    public static int minus(int a, int b) {
+        //write your code here
+        return a-b;
+    }
+
+    public static int multiply(int a, int b) {
+        //write your code here
+        return a*b;
+    }
+
+    public static double divide(int a, int b) {
+        //write your code here
+        return (double) a/b;
+    }
+
+    public static double percent(int a, int b) {
+        //write your code here
+        return a*((double) b/100);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
