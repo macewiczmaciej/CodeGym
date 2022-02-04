5eb8c3039803a59eed10f5a7292a0168fb354ddf
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-02-04 16:27:47 +0100

    Create Imported code.java

---
+++ Imported code.java
@@ -0,0 +1,14 @@
+package en.codegym.task.pro.task11.task1118;
+
+/* 
+Imported code
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        java.util.Scanner console = new java.util.Scanner(System.in);
+        if (console.hasNext()) {
+            System.out.println(console.next());
+        }
+    }
+}
