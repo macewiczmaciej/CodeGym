195fef88de5b434d39e9b86dd8de93d87eff2047
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-02-16 22:59:14 +0100

    Create In decreasing order.java

---
+++ In decreasing order.java
@@ -0,0 +1,35 @@
+package en.codegym.task.jdk13.task07.task0728;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/* 
+In decreasing order
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+        //write your code here
+        Arrays.sort(array);
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+    }
+}
