453447a6e73368c04821d0664ccbf06a431cbcaf
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-02-17 22:35:34 +0100

    Create HashMap of Objects.java

---
+++ HashMap of Objects.java
@@ -0,0 +1,31 @@
+package en.codegym.task.jdk13.task08.task0806;
+
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+HashMap of Objects
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("Sim", 5);
+        map.put("Tom", 5.5);
+        map.put("Arbus", false);
+        map.put("Baby", null);
+        map.put("Cat", "Cat");
+        map.put("Eat", new Long(56));
+        map.put("Food", new Character('3'));
+        map.put("Gevey", '6');
+        map.put("Hugs", 111111111111L);
+        map.put("Comp", (double) 123);
+
+        //write your code here
+        for (Map.Entry<String, Object> hash : map.entrySet()) {
+            System.out.println(hash.getKey() + " - " + hash.getValue());
+        }
+
+    }
+}
