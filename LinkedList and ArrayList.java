d98ac948a77e4cd740b398c29506faba0148f6ed
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-02-17 22:47:15 +0100

    Create LinkedList and ArrayList.java

---
+++ LinkedList and ArrayList.java
@@ -0,0 +1,27 @@
+package en.codegym.task.jdk13.task08.task0807;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/* 
+LinkedList and ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        //write your code here
+        ArrayList array = new ArrayList();
+        return array;
+    }
+
+    public static Object createLinkedList() {
+        //write your code here
+        LinkedList linked = new LinkedList();
+        return linked;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
