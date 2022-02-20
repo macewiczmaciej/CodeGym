4e1eeb1063e436f258d78b670b334064cd7c5e74
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-02-20 20:22:54 +0100

    Create 20 words that start with the letter "L".java

---
+++ 20 words that start with the letter "L".java
@@ -0,0 +1,39 @@
+package en.codegym.task.jdk13.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+20 words that start with the letter "L"
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        Set<String> set = new HashSet<>();
+        set.add("London");
+        set.add("Label");
+        set.add("Lack");
+        set.add("Landing");
+        set.add("Lane");
+        set.add("Last");
+        set.add("Line");
+        set.add("Left");
+        set.add("Laugh");
+        set.add("Lately");
+        set.add("Later");
+        set.add("Large");
+        set.add("Landlord");
+        set.add("Landfill");
+        set.add("Ladder");
+        set.add("Lady");
+        set.add("Language");
+        set.add("Lecture");
+        set.add("Leave");
+        set.add("Learn");
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
