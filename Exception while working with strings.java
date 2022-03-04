a6e55861436dda40d33d449b8c1e2dbefb13319a
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-03-04 19:35:19 +0100

    Create Exception while working with strings.java

---
+++ Exception while working with strings.java
@@ -0,0 +1,20 @@
+package en.codegym.task.jdk13.task09.task0908;
+
+/* 
+Exception while working with strings
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+        try{
+            String s = null;
+            String m = s.toLowerCase();
+        }catch (NullPointerException e){
+            System.out.println(e);
+        }
+
+        //write your code here
+    }
+}
