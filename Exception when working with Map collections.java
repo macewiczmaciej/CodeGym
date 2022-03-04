6867555ec24065967f69965a2a1b44f1a07fb98f
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-03-04 19:39:59 +0100

    Create Exception when working with Map collections.java

---
+++ Exception when working with Map collections.java
@@ -0,0 +1,24 @@
+package en.codegym.task.jdk13.task09.task0911;
+
+import java.util.EmptyStackException;
+import java.util.HashMap;
+
+/* 
+Exception when working with Map collections
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //write your code here
+
+        try {
+            HashMap<String, String> map = new HashMap<>(null);
+            System.out.println(map);
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
+
+        //write your code here
+    }
+}
