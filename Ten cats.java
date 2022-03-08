70d56ddc66f2c2afb42fd6e0acf12b75a1261a66
Maciej Macewicz <52372101+macewiczmaciej@users.noreply.github.com>
2022-03-08 20:03:32 +0100

    Create Ten cats.java

---
+++ Ten cats.java
@@ -0,0 +1,66 @@
+package en.codegym.task.jdk13.task09.task0927;
+
+import java.util.*;
+
+/* 
+Ten cats
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Map<String, Cat> map = createMap();
+        Set<Cat> set = convertMapToSet(map);
+        printCatSet(set);
+    }
+
+    public static Map<String, Cat> createMap() {
+        //write your code here
+        Map<String, Cat> map = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            map.put("Cat" + i, new Cat("Cat" + i));
+        }
+        return map;
+    }
+
+    public static Set<Cat> convertMapToSet(Map<String, Cat> map) {
+        //write your code here
+
+        return new HashSet<>(map.values());
+    }
+
+    public static void printCatSet(Set<Cat> set) {
+        for (Cat cat : set) {
+            System.out.println(cat);
+        }
+    }
+
+    public static class Cat {
+        public String name;
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return "Cat " + this.name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Cat)) {
+                return false;
+            }
+            Cat cat = (Cat) o;
+            return Objects.equals(name, cat.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+}
